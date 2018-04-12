c39967d442a7e27d016189cbd836ce47e5fe4886
Sarah Lee <sarah89lee@Gmail.com>
2018-04-11 20:34:45 -0700

    Added tab bar controller with the three main view controllers. Added image assets.

---
+++ quotes/ViewControllers/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  quotes
+//
+//  Created by Sarah Lee on 4/11/18.
+//  Copyright © 2018 Sarah Lee. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ quotes/ViewControllers/QuotesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  QuotesViewController.swift
+//  quotes
+//
+//  Created by Sarah Lee on 4/11/18.
+//  Copyright © 2018 Sarah Lee. All rights reserved.
+//
+
+import UIKit
+
+class QuotesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
